f1c7575751d14d8b9948edb3e27b0129eae8d750
gavroshcho <nikolaivylchanov@gmail.com>
2017-03-23 16:58:05 +0200

    Make follow functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   acts_as_voter
 
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :following, through: :following_relationships, source: :following
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
@@ -12,4 +18,12 @@ class User < ApplicationRecord
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16  }
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   get ':user_name/edit',to: "profiles#edit", as: :edit_profile
   patch ':user_name/edit', to: 'profiles#update', as: :update_profile
   get 'notifications/:id/link_through', to: "notifications#link_through", as: :link_through
-
+  post ':user_name/follow_user', to: "relationships#follow_user", as: :follow_user
+  post ':user_name/unfollow_user', to: "relationships#unfollow_user", as: :unfollow_user
 end
